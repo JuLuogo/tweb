35374c71102a8e18e69f8168ed425892c2dc2fb2
JuLuogo <1576586736@qq.com>
2025-08-14 13:17:38 +0800

    feat: 添加多个辅助函数和类型定义
    新增了多个辅助函数，包括数组操作、字符串处理、DOM操作等工具函数，并添加了相关的类型定义和常量。同时引入了环境检测、加密工具、动画缓动函数等功能模块。这些改动为项目提供了更完善的工具链和类型支持。

---
+++ src/langSign.ts
@@ -0,0 +1,40 @@
+const lang = {
+  'Login.Title': 'Sign in to Telegram',
+  'Login.PhoneLabel': 'Phone Number',
+  'Login.PhoneLabelInvalid': 'Phone Number Invalid',
+  'Login.StartText': 'Please confirm your country code\nand enter your phone number.',
+  'Login.Code.SentSms': 'We have sent you an SMS\nwith the code.',
+  'Login.Code.SentInApp': 'We have sent you a message in Telegram\nwith the code.',
+  'Login.Code.SentCall': 'We will call you and dictate\nthe code.',
+  'Login.Code.SentUnknown': 'Please check everything\nfor a code (type: %s)',
+  'Login.Password.Title': 'Enter Your Password',
+  'Login.Password.Subtitle': 'Your account is protected with\nan additional password',
+  'Login.Register.Subtitle': 'Enter your name and add\na profile photo',
+  'PleaseWait': 'Please wait...',
+
+  // * android
+  'Code': 'Code',
+  'LoginPassword': 'Password',
+  'YourName': 'Your Name',
+  'FirstName': 'First name (required)',
+  'LastName': 'Last name (optional)',
+  'StartMessaging': 'Start Messaging',
+  'Country': 'Country',
+
+  // * macos
+  'Contacts.PhoneNumber.Placeholder': 'Phone Number',
+  'Login.Next': 'Next',
+  'Login.ContinueOnLanguage': 'Continue in English',
+  'Login.QR.Title': '扫码没法登，请通过手机号登录。如果码都出不来就是API炸了',
+  'Login.QR.Help1': '本项目仅供内部测试请勿外传。*避免滥用*。',
+  'Login.QR.Help2': '注意，将该项目绑定到的域名会吃Abuse，禁止外传。',
+  'Login.QR.Help3': '本项目不收取任何费用。',
+  'Login.QR.Cancel': '点击使用手机号登陆',
+  'Login.QR.Login': 'Log in by QR Code',
+  'PhoneNumber.Code.Fragment.Info': 'Get the code in the Anonymous Numbers section on [Fragment](%1$@)',
+  'PHONE_CODE_INVALID': 'Invalid code',
+  'PHONE_CODE_EXPIRED': 'Code expired',
+  'PASSWORD_HASH_INVALID': 'Incorrect password'
+};
+
+export default lang;
